e6056863991603ee348617be87126dd72258caad
Vertigo <Vertigo@DESKTOP-S2UGV0R>
2020-05-20 10:16:24 +0300

    UI java script

--- Code.js
+++ Code.js
@@ -1,13 +1,21 @@
 function onOpen() {
     let ui = SpreadsheetApp.getUi();
 
-    ui.createMenu('Custom Menu').addItem('Generate Link', 'showSidebar').addToUi();
+    ui.createMenu('Custom Menu').addItem('Generate Link', 'showMenu').addToUi();
 }
 
-function showSidebar() {
-    let html = HtmlService.createHtmlOutputFromFile('Menu')
-      .setHeight(600)
-      .setWidth(300);
+function showMenu() {
+    let html = HtmlService.createTemplateFromFile('Menu').evaluate().setWidth(300).setHeight(600);
 
     SpreadsheetApp.getUi().showModalDialog(html, 'Generate Link');
+}
+
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+
+function getSheets() {
+    let sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets().map((sheet) => sheet.getName());
+
+    return sheets;
 }
